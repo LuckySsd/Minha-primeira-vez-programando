5ff7142988cc439f8ebedf86c301429f21c3b969
Lucky <victorneves03@hotmail.com>
2022-11-14 20:43:05 -0300

    Minha tentativa/aula de programação

---
+++ Aula.js
@@ -0,0 +1,41 @@
+let hora = 7;
+
+if (hora > 6 && hora < 12) {
+    console.log('bom dia');
+}
+else if (hora > 12 && hora < 18) {
+    console.log('boa tarde');
+}
+else {
+    console.log('boa noite');
+};
+
+let usuario = '';
+
+switch (usuario) {
+    case 'comum' :
+    console.log('usuario comum');
+    break
+
+    case 'Chefe' :
+    console.log('usuario chefe');
+    break
+
+    case 'proprietario' :
+    console.log('usuario proprietario');
+    break
+
+    default :
+    console.log('usuario não reconhecido');
+}
+
+let deyvid = ''
+
+switch (deyvid) {
+    case 'meu cu' :
+    console.log('seu cu foi aceito')
+    break
+
+    default :
+    console.log('vixe man sai fora')
+};
